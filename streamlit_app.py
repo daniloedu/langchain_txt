35b5c0de6f1941475266164e3cec2035bf4d4cfb
Danilo Salazar <danilo@origo.ec>
2023-07-06 18:27:32 -0400

    Update streamlit_app.py
    changing the logic for the form

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ result = []
 with st.form('myform', clear_on_submit=True):
     #openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=no (uploaded_file and query_text))
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
-    if submitted and openai_api_key.startswith('sk-'):
+    if submitted # and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
             response = generate_response(uploaded_file, openai_api_key, query_text)
             result.append(response)
